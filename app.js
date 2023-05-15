d2def833fab79f44c46f292387e7b9d5d3342b1a
Ramachandran Varadaraju <rajram92@hotmail.com>
2023-05-15 12:36:30 +0530

    Template engine ejs, pug, hanglebars added

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const path = require("path");
 
 const express = require("express");
+// const { engine } = require("express-handlebars");
 
 const rootDir = require("./util/path");
 
 const app = express();
 
+// Pug template engine
+// app.set("view engine", "pug");
+// app.set("views", "views");
+
+// Handlebars template engine
+// app.engine(
+//   "hbs",
+//   engine({
+//     layoutsDir: "views/layouts",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
+// app.set("view engine", "hbs");
+// app.set("views", "views");
+
+//  EJS template engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 // Import routes
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 // access to static files
@@ -20,13 +41,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // admin routes
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 
-// homepage
+// homepage routes
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  // res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404", { docTitle: "Page not found", path: "" });
 });
 
 app.listen(3000, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -6,12 +6,23 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    docTitle: "Add product",
+    path: "/admin/add-product",
+    activeProduct: true,
+    productCSS: true,
+    formsCSS: true,
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
